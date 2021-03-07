34a839d465b8b268960681be483fd7682f19a799
Thomas Tannhaeuser (q498620) <thomas.tannhaeuser@bmw.de>
2021-03-07 20:52:39 +0100

    add say endpoint and cleanup

--- app.py
+++ app.py
@@ -4,39 +4,70 @@ import io
 from contextlib import redirect_stdout
 import random
 
-app = Flask('cowsay', template_folder='/home/q498620/projects/udacity/udacity-devops-capstone/templates')
+import pathlib
+import os.path as path
+
+app = Flask('cowsay', template_folder=path.join(pathlib.Path(__file__).parent.absolute(), 'templates'))
 
 SERVICE_VERSION = '0.0.1'
 
 class UnknownSayerException(Exception):
     pass
 
-def capture_say(what, who='cow'):
+def capture_say(message, speaker='cow'):
 
-    if who == 'random':
-        who = random.choice(cowsay.char_names)
+    if speaker == 'random':
+        speaker = random.choice(cowsay.char_names)
 
-    if who not in cowsay.char_names:
-        raise UnknownSayerException(f'sayer {who} not known to me')
+    if speaker not in cowsay.char_names:
+        raise UnknownSayerException(f"sayer '{speaker}' not known to me")
 
-    sayer_call = getattr(cowsay, who)
+    speaker_say = getattr(cowsay, speaker)
 
     f = io.StringIO()
     with redirect_stdout(f):
-        sayer_call(what)
+        speaker_say(message)
     return f.getvalue()
+
+def build_err_40x(message, speaker='random', http_status=404):
+    resp_text = capture_say(message, speaker=speaker)
+    resp = make_response(render_template('40x.html', almighty_content=resp_text, http_status=http_status), http_status)
+    return resp
+
+def build_response(message, speaker='cow'):
+    
+    try:
+        resp_text = capture_say(message, speaker=speaker)
+        resp = make_response(render_template('index.html', almighty_content=resp_text), 200)
+        return resp
+        
+    except UnknownSayerException as err:
+        return build_err_40x(str(err))
 
 @app.route('/')
 def index():
-    resp_text = capture_say(f'COWSAY service {SERVICE_VERSION}')
-    resp = make_response(render_template('index.html', almighty_content=resp_text), 200)
-    return resp
+    
+    return build_response(f'COWSAY service {SERVICE_VERSION}')
+
+@app.route('/say', methods=['POST'])
+def say():
+
+    try:
+        message = request.form['message'],
+        speaker = request.form['speaker']
+        return build_response(message, speaker=speaker)
+    except Exception as err:
+        return build_err_40x(str(err), http_status=400)
+
+@app.route('/speakers')
+def speakers():
+    cowsay_speakers = '\n'.join(cowsay.char_names)
+
+    return build_response(f'available speakers:\n\n{cowsay_speakers}')
 
 @app.errorhandler(404)
 def not_found(error):
 
-    resp_text = capture_say("404! The path '{}' was not found on this server.".format(request.path), who='random')
-    resp = make_response(render_template('404.html', almighty_content=resp_text), 404)
-    return resp
+    return build_err_40x("404! The path '{}' was not found on this server.".format(request.path))
 
 app.run(port=8080)
